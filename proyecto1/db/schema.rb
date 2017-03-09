35b88dac05a7acfab46501b68d47da6d24cf4c51
Eder Camilo Muñoz Florez <cmunozf@eafit.edu.co>
2017-03-09 07:21:52 -0500

    Revirtiendo xD

--- proyecto1/db/schema.rb
+++ proyecto1/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170204221629) do
 
-  create_table "admins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -25,11 +25,11 @@ ActiveRecord::Schema.define(version: 20170204221629) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "libros", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "libros", force: :cascade do |t|
     t.string   "titulo"
     t.string   "autor"
     t.integer  "fecha_pub"
@@ -37,5 +37,22 @@ ActiveRecord::Schema.define(version: 20170204221629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  # create_table "users", force: :cascade do |t|
+  #   t.string   "email",                  default: "", null: false
+  #   t.string   "encrypted_password",     default: "", null: false
+  #   t.string   "reset_password_token"
+  #   t.datetime "reset_password_sent_at"
+  #   t.datetime "remember_created_at"
+  #   t.integer  "sign_in_count",          default: 0,  null: false
+  #   t.datetime "current_sign_in_at"
+  #   t.datetime "last_sign_in_at"
+  #   t.string   "current_sign_in_ip"
+  #   t.string   "last_sign_in_ip"
+  #   t.datetime "created_at",                          null: false
+  #   t.datetime "updated_at",                          null: false
+  #   t.index ["email"], name: "index_users_on_email", unique: true
+  #   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  # end
 
 end
